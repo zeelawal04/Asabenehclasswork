e4258925f0b6d3609949a82ad2924dc630e18d9f
Zainab Lawal <zainabdamilolalawal@gmail.com>
2022-08-08 16:17:43 +0100

    feat:Asabeneh

---
+++ script.js
@@ -0,0 +1,51 @@
+//no1
+let quote =
+  "'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+
+console.log(quote);
+
+//no2
+quote =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+
+console.log(quote);
+
+//no3
+let number = 10;
+let string = "10";
+console.log(number == string);
+
+//no4
+let num = 9.8;
+console.log(Math.round(num));
+
+//no5
+let py = "python";
+let jr = "jargon";
+console.log(py.includes("on") && jr.includes("on"));
+
+//no6
+let word = "I hope this course is not full of jargon";
+console.log(word.includes("jargon"));
+
+//n07
+let numb = Math.floor(Math.random() * 101);
+console.log(numb);
+
+//n08
+numb = Math.floor(Math.random() * 50) + 50;
+console.log(numb);
+
+//n09
+numb = Math.floor(Math.random() * 256);
+console.log(numb);
+
+//no10
+console.log(Math.random("JavaScript"));
+
+//no11
+
+//no12
+let eng =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(eng.substr(31, 23));
